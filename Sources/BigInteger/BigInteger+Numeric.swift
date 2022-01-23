6cc0f1d52f102d3c7ea37a154fa292cb699c735c
Logan Richards <sdrahcir.log@me.com>
2022-01-23 01:38:32 -0700

    BigInteger Conform to SignedNumeric

--- Sources/BigInteger/BigInteger+Numeric.swift
+++ Sources/BigInteger/BigInteger+Numeric.swift
@@ -61,5 +61,51 @@ extension BigInteger: Numeric {
 }
 
 //TODO: SignedNumeric
+extension BigInteger: SignedNumeric {
+    /// Returns the additive inverse of the specified value.
+    ///
+    /// The negation operator (prefix `-`) returns the additive inverse of its
+    /// argument.
+    ///
+    ///     let x = BigInt(21)
+    ///     let y = -x
+    ///     // y == -[1, 2] == -21
+    ///
+    /// - Returns: The additive inverse of the argument.
+    prefix public static func - (operand: BigInteger) -> BigInteger {
+        var result = operand
+        result.negate()
+        return result
+    }
+    
+    /// Replaces this value with its additive inverse.
+    ///
+    /// The following example uses the `negate()` method to negate the value of
+    /// an integer `x`:
+    ///
+    ///     var x = 21
+    ///     x.negate()
+    ///     // x == -21
+    public mutating func negate() {
+        negative.toggle()
+    }
+    
+    /// Returns the given number unchanged.
+    ///
+    /// You can use the unary plus operator (`+`) to provide symmetry in your
+    /// code for positive numbers when also using the unary minus operator.
+    ///
+    ///     let x = -21
+    ///     let y = +21
+    ///     // x == -21
+    ///     // y == 21
+    ///
+    /// - Returns: The given argument without any changes.
+    prefix public static func + (x: BigInteger) -> BigInteger {
+        return x
+    }
+}
+
+
 //TODO: BinaryInteger
 //TODO: SignedInteger
